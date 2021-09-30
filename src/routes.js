130e339e9de295cf69d964309723be2b616dd2e8
Gabriel Albino <enggabrielalbino@gmail.com>
2021-09-30 14:41:55 -0300

    Adicionando fluxo de recuperação de senha

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,9 @@ const { NODE_ENV } = process.env;
 
 routes.post('/login', UserController.signIn);
 routes.post('/register', UserController.register);
+routes.post('/forgot_password', UserController.forgot_password);
+routes.post('/reset_password', UserController.reset_password);
 routes.put('/update', UserController.loginRequired, UserController.updateUser)
+
 
 module.exports = routes;
