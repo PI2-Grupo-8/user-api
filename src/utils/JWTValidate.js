1a38e32ded36077d1d230b92e52bfce4325b7498
Gabriela Guedes <gabrielabguedes@gmail.com>
2021-10-21 22:52:38 -0300

    Add secret on .env
    Signed-off-by: Gabriela Guedes <gabrielabguedes@gmail.com>

--- src/utils/JWTValidate.js
+++ src/utils/JWTValidate.js
@@ -1,8 +1,9 @@
 const jsonwebtoken = require("jsonwebtoken");
+const { SECRET } = process.env
 
 const JWTValidate = (req, res, next) => {
   if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
-    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function (err, decode) {
+    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], SECRET, function (err, decode) {
       if (err) req.user = undefined;
       req.user = decode;
       next();
